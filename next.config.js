a145374a766643c42421e75676ac77ab6f0a553e
Franke Telgenhof <franke.telgenhof@postcodeloterij.nl>
2023-06-27 15:32:57 +0200

    Use app router instead of pages router

---
+++ next.config.js
@@ -0,0 +1,8 @@
+process.env.__NEXT_PRIVATE_PREBUNDLED_REACT = 'next';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = {
+    ...nextConfig,
+};
--- src/Redux/Reducer/AppReducer.ts
+++ src/Redux/Reducer/AppReducer.ts
@@ -1,15 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export const appSlice = createSlice({
-  name: "app",
-  initialState: {
-    showResult: false,
-  },
-  reducers: {
-    toggleShowResult: (state) => {
-      state.showResult = !state.showResult;
+    name: 'app',
+    initialState: {
+        showResult: false,
     },
-  },
+    reducers: {
+        toggleShowResult: (state) => {
+            state.showResult = !state.showResult;
+        },
+    },
 });
 
 export const { toggleShowResult } = appSlice.actions;
